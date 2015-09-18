51052113deb4f03300e9c9313b3845d5293f9970
Boyko Anton <boyko.ant@live.com>
2015-09-18 20:43:33 +0300

    adjust gulp zip-dist task

--- gulp/zip-dist.js
+++ gulp/zip-dist.js
@@ -4,9 +4,10 @@ var path = require('path');
 var gulp = require('gulp');
 var conf = require('./conf');
 var zip = require('gulp-zip');
+var ignore = require('gulp-ignore');
 
 gulp.task('zip-dist', function () {
-  return gulp.src(path.join(conf.paths.dist, '/**'))
+  return gulp.src([path.join(conf.paths.dist, '/**'), '!./**/*.map'])
     .pipe(zip('dist.zip'))
     .pipe(gulp.dest(conf.paths.dist));
 });
